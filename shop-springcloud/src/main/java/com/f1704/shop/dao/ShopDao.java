c228c8207f7628979dddcf28ddfb964d8fa3f2fd
huxiaoxing <1072840952@qq.com>
2019-12-03 13:51:10 +0800

    Initial commit

---
+++ shop-springcloud/src/main/java/com/f1704/shop/dao/ShopDao.java
@@ -0,0 +1,20 @@
+package com.f1704.shop.dao;
+
+import com.f1704.shop.model.Shop;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface ShopDao {
+  List<Shop> selectList(@Param("shop") Shop shop);
+
+  int insert(@Param("shop") Shop shop);
+  int update(@Param("shop")Shop shop);
+  @Select("select * from shop where id=#{id}")
+  Shop getDetailById(@Param("id") Integer id);
+  @Delete("delete from shop where id in (${ids})")
+  int delByIds(@Param("ids") String ids);
+}
